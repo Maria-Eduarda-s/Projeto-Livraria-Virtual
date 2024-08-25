d6e941ef5c298f684f4a0c1e06c0d6ce55c58afc
Leandro <leandroalmeidaoliveira650@gmail.com>
2024-08-25 09:59:16 -0300

    arruma classe venda

--- src/Livro/Venda.java
+++ src/Livro/Venda.java
@@ -3,34 +3,42 @@ import java.util.List;
 
 public class Venda {
     private static int numVendas = 0;
-    private int numero;
+    private final int numero;
     private String cliente;
-    private float valor;
-    private List<Livro> livros;
+    private double valor;
+    private Livro[] livros;
 
-    public Venda(String cliente, float valor) {
+    public Venda(String cliente, int maxLivros) {
         this.numero = ++numVendas;
         this.cliente = cliente;
-        this.valor = valor;
-        this.livros = new ArrayList<>();
+        this.valor = 0.0;
+        this.livros = new Livro[maxLivros];
     }
 
     public void addLivro(Livro l, int index) {
-        if (index >= 0 && index <= livros.size()) {
-            livros.add(index, l);
+        if (index >= 0 && index < livros.length) {
+            if (livros[index] == null) {
+                livros[index] = l;
+                valor += l.getPreco();
+            } else {
+                System.out.println("Ja existe um livro nessa posicao.");
+            }
         } else {
-            System.out.println("Índice inválido!");
+            System.out.println("indice fora dos limites do array.");
         }
     }
 
     public void listarLivros() {
-        if (livros.isEmpty()) {
-            System.out.println("Nenhum livro à venda.");
-        } else {
-            System.out.println("Livros à venda:");
-            for (Livro livro : livros) {
+        boolean livrosEncontrados = false;
+        System.out.println("Livros comprados:");
+        for (Livro livro : livros) {
+            if (livro != null) {
                 System.out.println(livro);
+                livrosEncontrados = true;
             }
+        }
+        if (!livrosEncontrados) {
+            System.out.println("Nenhum livro foi vendido.");
         }
     }
 
@@ -46,11 +54,19 @@ public class Venda {
         this.cliente = cliente;
     }
 
-    public float getValor() {
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
         return valor;
     }
 
-    public void setValor(float valor) {
-        this.valor = valor;
+    public Livro[] getLivros() {
+        return livros;
+    }
+
+    public static int getNumVendas() {
+        return numVendas;
     }
 }
